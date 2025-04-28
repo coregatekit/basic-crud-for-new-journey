907e2ecb476fa49bd9f50167da0e6a6021d0e6fe
Joe Kim <joesm.genie@gmail.com>
2025-04-28 17:02:18 +0700

    add entity employee

---
+++ src/entities/employee.ts
@@ -0,0 +1,34 @@
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'employees'})
+export class Employee {
+  @Column({ primary: true, generated: 'uuid' })
+  id!: string;
+
+  @Column({ name: 'code', type: 'varchar', length: 10, nullable: false })
+  code!: string;
+
+  @Column({ name: 'salary', type: 'decimal', precision: 10, scale: 2, nullable: false })
+  salary!: number;
+
+  @Column({ name: 'varchar', length: 100, nullable: false })
+  email!: string;
+
+  @Column({ name: 'started_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  startedAt!: Date;
+
+  @Column({ name: 'person_id', type: 'uuid', nullable: false })
+  personId!: string;
+
+  @Column({ name: 'position_id', type: 'int', nullable: false })
+  positionId!: number;
+
+  @Column({ name: 'active', type: 'boolean', default: true })
+  active!: boolean;
+
+  @Column({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt!: Date;
+
+  @Column({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt!: Date;
+}
