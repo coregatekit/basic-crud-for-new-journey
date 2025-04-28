0c97bf69668d70afa246fc7ad2670149b3f2b42c
Joe Kim <joesm.genie@gmail.com>
2025-04-28 16:58:24 +0700

    update on update person

--- src/entities/person.ts
+++ src/entities/person.ts
@@ -26,6 +26,6 @@ export class Person {
   @Column({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt!: Date;
 
-  @Column({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
   updatedAt!: Date;
 }
