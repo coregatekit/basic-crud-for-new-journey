c5702ec6a4f157350d0968e21a8c9dd60b4fc0d0
Joe Kim <joesm.genie@gmail.com>
2025-04-30 17:26:22 +0700

    implement route update employee

--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -1,12 +1,12 @@
-import { Router, type Request, type Response } from 'express';
-import employeeRepository from '../repositories/empoyee.repository';
-import { generateEmployeeCode } from '../utils/generator';
-import positionRepository from '../repositories/position.repository';
-import personRepository from '../repositories/person.repository';
+import { Router, type Request, type Response } from "express";
+import employeeRepository from "../repositories/empoyee.repository";
+import { generateEmployeeCode } from "../utils/generator";
+import positionRepository from "../repositories/position.repository";
+import personRepository from "../repositories/person.repository";
 
 export const employeeRouter = Router();
 
-employeeRouter.get("/" , async (_req: Request, res: Response) => {
+employeeRouter.get("/", async (_req: Request, res: Response) => {
   const employees = await employeeRepository.find();
   if (employees.length === 0) {
     res.status(404).json({ message: "No employees found" });
@@ -98,4 +98,33 @@ employeeRouter.post("/", async (req: Request, res: Response) => {
 
   await employeeRepository.save(employee);
   res.status(201).json(employee);
+});
+
+employeeRouter.patch("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400).json({ message: "ID is required" });
+    return;
+  }
+
+  const employee = await employeeRepository.findOneBy({ id });
+  if (!employee) {
+    res.status(404).json({ message: "Employee not found" });
+    return;
+  }
+
+  const { salary, email, active } = req.body;
+
+  if (salary) {
+    employee.salary = Number(salary);
+  }
+  if (email) {
+    employee.email = String(email);
+  }
+  if (active) {
+    employee.active = Boolean(active);
+  }
+
+  await employeeRepository.save(employee);
+  res.status(200).json(employee);
 });
