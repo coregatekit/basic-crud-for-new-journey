d3ba8dc9ec05f19ee71befb46ee6619e3b2ffb6c
Joe Kim <joesm.genie@gmail.com>
2025-04-30 17:02:32 +0700

    implement route get all employees

--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -1,3 +1,14 @@
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
+import employeeRepository from '../repositories/empoyee.repository';
 
 export const employeeRouter = Router();
+
+employeeRouter.get("/" , async (_req: Request, res: Response) => {
+  const employees = await employeeRepository.find();
+  if (employees.length === 0) {
+    res.status(404).json({ message: "No employees found" });
+    return;
+  }
+
+  res.status(200).json(employees);
+});
