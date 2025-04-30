062bd9f5e6507aab8af76e1127139ba4d8f804a5
Joe Kim <joesm.genie@gmail.com>
2025-04-30 16:40:39 +0700

    implement route get all persons

--- src/routes/person.route.ts
+++ src/routes/person.route.ts
@@ -1,7 +1,15 @@
 import { Router, type Request, type Response } from "express";
+import personRepository from '../repositories/person.repository';
 
 export const personRouter = Router();
 
 personRouter.get("/", async (_req: Request, res: Response) => {
-  res.status(501).json({ message: "Not implement yet!" });
+  const persons = await personRepository.find();
+
+  if (persons.length === 0) {
+    res.status(404).json({ message: "No persons found" });
+    return;
+  }
+
+  res.status(200).json(persons);
 });
